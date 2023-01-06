b9726a1129b670a935598152216aa397986fc834
Lucas BRUEL <lucasfox@outlook.fr>
2023-01-06 10:51:06 +0100

    [New] Modification de la fonction afin d'informer su rle mail non remplie

--- js/nolark.js
+++ js/nolark.js
@@ -1,12 +1,13 @@
 function validEnvoi() {
- if (window.document.querySelector("#i_nom").value === "" &&
- window.document.querySelector("#i_prenom").value === "" ) {
- alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
- }
- else {
- window.document.querySelector("#form_contact").submit(); // On peut envoyer
- }
+    if (window.document.querySelector("#i_nom").value === "" &&
+            window.document.querySelector("#i_prenom").value === "") {
+        alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+    } else if (window.document.querySelector("#i_email").value === "") {
+        alert("L'email doit être rempli"); // On affiche un message
+    } else {
+        window.document.querySelector("#form_contact").submit(); // On peut envoyer
+    }
 }
-window.addEventListener("load", function() {
- window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+window.addEventListener("load", function () {
+    window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
 });
