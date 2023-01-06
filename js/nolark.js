bcb17dc76244b733e6e2e0f61a22c96d3847ea6c
Lucas BRUEL <lucasfox@outlook.fr>
2023-01-06 10:32:21 +0100

    [New] Ajout d'une fonction "validEnvoie" javascript pour la page Nous-contacter

--- js/nolark.js
+++ js/nolark.js
@@ -1,2 +1,12 @@
-
-
+function validEnvoi() {
+ if (window.document.querySelector("#i_nom").value === "" &&
+ window.document.querySelector("#i_prenom").value === "" ) {
+ alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+ }
+ else {
+ window.document.querySelector("#form_contact").submit(); // On peut envoyer
+ }
+}
+window.addEventListener("load", function() {
+ window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+});
