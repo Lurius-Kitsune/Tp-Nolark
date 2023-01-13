15dfa9ea13e568c51219398824b158227fd75d86
Lucas BRUEL <lucasfox@outlook.fr>
2023-01-13 11:08:44 +0100

    [New] Fonction majoration

--- js/remun.js
+++ js/remun.js
@@ -1,5 +1,23 @@
 const fix = 1100; // Rémunération fixé sans majoration d'anciennet
-let nbAncien = 6;
+let nbAncien = parseInt(window.document.querySelector("#num_ancien").value); // Int
+let salaire = 0;
+
+
+
+
+function majoration(nbAncien){
+        if (nbAncien >= 5){
+        for(i = 0; i === nbAncien; i++){
+            if (i >= 5 && i < 10){
+                salaire += fix * 1.03;
+            }
+            else if(i >= 10){
+                salaire += fix * 1.06;
+            }
+        }
+    }
+}
+
 
 
 
