be4df433e19c5018d57a52f1193d23975512bbc6
Lucas BRUEL <lucasfox@outlook.fr>
2023-01-13 15:16:34 +0100

    [Patch] Erreur dans majoration (multiplication au lieux addition)

--- js/remun.js
+++ js/remun.js
@@ -11,21 +11,21 @@ class Casque {
     constructor(nom, prix, taux, qteRq) {
         this.nom = nom;
         this.prix = prix;
-        this.taux =  taux;
-        this.qteRq = [...qteRq,99999999];
+        this.taux = taux;
+        this.qteRq = [...qteRq, 99999999];
     }
-    
+
     /***
      * 
      * @param {int} qte La quantité de casque vendu
      * @returns {Number} Renvoie la comission toucher à la vente total de Casque
      */
-    Commission(qte){
+    Commission(qte) {
         let total = 0;
         let i, j;
-        for(i=1; i!==qte+1; i++){
-            for(j = 0; j!==this.taux.length; j++) {
-                if (this.qteRq[j]+1<qte<this.qteRq[j+1]){
+        for (i = 1; i !== qte + 1; i++) {
+            for (j = 0; j !== this.taux.length; j++) {
+                if (this.qteRq[j] + 1 < qte < this.qteRq[j + 1]) {
                     total += this.prix + this.taux[j];
                 }
             }
@@ -34,11 +34,11 @@ class Casque {
     }
 }
 
-let Multi = new Casque("Multitec",180 ,[0.04 , 0.06, 0.1], [20,50]);
-let XSpirit = new Casque("X-Spirit",350 ,[0.06],[50]);
-let s20 = new Casque("S-20",140, [0.02],[0]);
+let Multi = new Casque("Multitec", 180, [0.04, 0.06, 0.1], [20, 50]);
+let XSpirit = new Casque("X-Spirit", 350, [0.06], [50]);
+let s20 = new Casque("S-20", 140, [0.02], [0]);
 
-salaire = fix + majoration(50) + Multi.Commission(50)+ s20.Commission(50)+ XSpirit.Commission(50);
+salaire = fix + majoration(50) + Multi.Commission(1) + s20.Commission(1) + XSpirit.Commission(1);
 
 /**
  * 
@@ -48,12 +48,10 @@ salaire = fix + majoration(50) + Multi.Commission(50)+ s20.Commission(50)+ XSpir
 function majoration(nbAncien) {
     let total = 0;
     if (nbAncien >= 5) {
-        for (i = 0; i !== nbAncien; i++) {
-            if (i >= 5 && i < 10) {
-                total += fix * 0.03;
-            } else if (i >= 10) {
-                total += fix * 0.06;
-            }
+        if (nbAncien >= 5 && nbAncien < 10) {
+            total += fix * 0.03;
+        } else {
+            total += fix * 0.06;
         }
     }
     return total;
