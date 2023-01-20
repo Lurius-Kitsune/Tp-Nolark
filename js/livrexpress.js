f97c906554ba6264b4500406ae9ec8e7d508df00
Lucas BRUEL <lucasfox@outlook.fr>
2023-01-20 15:10:29 +0100

    [Edit] Ajout de commentaire pour la fonction

--- js/livrexpress.js
+++ js/livrexpress.js
@@ -2,6 +2,12 @@ function primeAnnées(){
     
 }
 
+/**
+ * Affiche la prime obtenue en fonction de la distance parcourue:
+ * Cependant la valeur max sera le plafond !
+ * @param {Integer} dist
+ * @returns {float} La prime obtenue 
+ */
 function primeDist(dist){
     const plafond = 900, centpKm = 0.1;
     let i, result = 0;
