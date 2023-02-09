a5dd941929997f9c677f4e60ee575cf7d4909d41
Lucas BRUEL <lucasfox@outlook.fr>
2023-02-09 23:47:50 +0100

    [Refactor] Methode getAlcolemie à passer les test unitaire

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -15,15 +15,28 @@ function getAlcoolPur(nbVerres) {
  * @param {int} sexe
  * @returns {float}
  */
-function getCoefDiffusion(sexe){
-    if (sexe === "homme"){
+function getCoefDiffusion(sexe) {
+    if (sexe === "homme") {
         return 0.7;
-    }
-    else{
+    } else {
         return 0.6;
     }
 }
 
-function getAlcoolemie(sexe, poids, nbVerres){
-    return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+/**
+ * Fonction qui retourne l'alcoolémie en fonction du sexe, du poids et du 
+ * nombre de verres ingérés 
+ * 
+ * @param {string} sexe 
+ * @param {int} poids 
+ * @param {int} nbVerres 
+ * @returns {float} 
+ */
+function getAlcoolemie(sexe, poids, nbVerres) {
+    // /!\ division par 0, on ne veut pas provoquer la destruction de l'univers ;o) 
+    if (poids > 0) {
+        return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+    } else {
+        return 0;
+    }
 }
