df35febee3e58f16983dfb140bfc1e6ba6a64d3d
Lucas BRUEL <lucasfox@outlook.fr>
2023-02-09 23:00:31 +0100

    [New] Ajout du Test Unitaire pour la methode alcoolemie.js

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -1,9 +1,3 @@
-/**
-* Tests unitaires du projet Alcoolémie
-*
-*/
-MesTestsUnitaires = TestCase('AlcoolemieTest');
-MesTestsUnitaires.prototype.testsGetAlcoolPur = function() {
- assertEquals('0 verre', 0, getAlcoolPur(0));
- assertEquals('1 verre', 10, getAlcoolPur(1));
-};
+function getAlcoolPur(nbVerres) {
+ return nbVerres * 10;
+}
