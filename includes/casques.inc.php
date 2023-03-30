9874f2336094f769878a1ce27ed6a604f26fe432
Lucas BRUEL <lucasfox@outlook.fr>
2023-03-30 23:46:48 +0200

    [New] Ajout de la requête SQL afin d'afficher les casque

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,7 +1,21 @@
 <?php
+//COnnexion au SGBD
+$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+// Creation de la requête
+$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
+$req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
+$req .= ' INNER JOIN marque ON casque.marque=marque.id';
 
+// Envoie de la requête
+$res = $cnx->query($req);
+
+// Affichage du resultat
+echo '<section id="casques">';
+while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
+echo '<article>';
+echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
+'" alt="', $ligne->modele, '">';
+echo '</article>';
+}
+echo '</section>';
