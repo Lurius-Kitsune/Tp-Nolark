1a30bacda2622f0305097c45fe79420e06353544
Lucas BRUEL <lucasfox@outlook.fr>
2023-03-31 00:06:49 +0200

    [New] Ajout de l'affichage du stock des casques

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 //COnnexion au SGBD
 $cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
 
@@ -13,12 +14,21 @@ $res = $cnx->query($req);
 // Affichage du resultat
 echo '<section id="casques">';
 while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
-echo '<article>';
-echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-'" alt="', $ligne->modele, '">';
-echo '<p class="prix">', $ligne->prix, '</p>';
-echo '<p class="marque">', $ligne->nom, '</p>';
-echo '<p class="modele">', $ligne->modele, '</p>';
-echo '</article>';
+    echo '<article>';
+    echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
+    '" alt="', $ligne->modele, '">';
+    echo '<p class="prix">', $ligne->prix, ' €</p>';
+    if ($ligne->stock <= 0){
+        echo'<p class="stockko"><abbr data-tip="Sur commande uniquement">stock</abbr></p>';
+    }
+    else if ($ligne->stock <= 5){
+        echo'<p class="stockok"><abbr data-tip="Plus que ', $ligne->stock,' casques en stock...">stock</abbr></p>';
+    }
+    else {
+        echo'<p class="stockok"><abbr data-tip="', $ligne->stock,' casques en stock">stock</abbr></p>';
+    }
+    echo '<p class="marque">', $ligne->nom, '</p>';
+    echo '<p class="modele">', $ligne->modele, '</p>';
+    echo '</article>';
 }
 echo '</section>';
