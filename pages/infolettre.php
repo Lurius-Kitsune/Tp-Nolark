97bf896a2d18d0d0eb4063a8f6a1bcc1abd4094f
Lucas BRUEL <lucasfox@outlook.fr>
2023-04-07 11:35:42 +0200

    [New] Ajout de la page NewLetter

---
+++ pages/infolettre.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!-- 
+     Page web créé dans le cadre du cours de web Dev le 01/09/2018
+     Auteur : José GIL
+     Email : jgil83000@gmail.com
+-->
+
+<html lang="fr-FR">
+    <head>
+        <title>Casques Nolark : Sécurité et confort, nos priorités !</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="José GIL">
+        <meta name="description" content="Découvrez des casques moto dépassant même les exigences des tests de sécurité. Tous les casques Nolark au meilleur prix et avec en prime la livraison gratuite !">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/styles.css" rel="stylesheet" type="text/css">
+        <link href="../css/casques.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../favicon.ico">
+    </head>
+    <body>
+        <?php
+        include('../includes/header.html.inc.php');
+        ?>
+        <?php
+        include('../includes/footer.inc.php');
+        ?>
+    </body>
+</html>
