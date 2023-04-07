9cf7b19be8f4fb4576fddad835b7c55c16b8455c
Lucas BRUEL <lucasfox@outlook.fr>
2023-04-07 11:20:59 +0200

    [Training Epreuve E5] AJout d'un fichier infolettre.inc.php afin de communiquer avec la bd

---
+++ includes/infolettre.inc.php
@@ -0,0 +1,62 @@
+<?php
+
+function SetInfoLettre($mail){
+    $req = "INSERT INTO NEWSLETTER"
+    . "VALUES :mail";
+    try{
+        $nolarkBD = new PDO(
+                'mysql:host=localhost;dbname=nolark;charset=utf8',
+                'nolarkadmin',
+                'nolarkadmin'
+        );
+        
+        $resultat = $nolarkBD ->prepare($req);
+        $resultat->execute([
+            'mail' => $mail,
+        ]);
+        echo $resultat ->fetchAll();
+    } catch (Exception $ex) {
+        die('Erreur : ' . $ex->getMessage());
+    }
+}
+
+function SetConsentement($mail){
+    $req = "UPDATE NEWSLETTER"
+           ."SET consentement = True, datereceuil = CURRENT_TIMESTAMP()"
+           ."where mail = :mail";
+    try{
+        $nolarkBD = new PDO(
+                'mysql:host=localhost;dbname=nolark;charset=utf8',
+                'nolarkadmin',
+                'nolarkadmin'
+        );
+        
+        $resultat = $nolarkBD ->prepare($req);
+        $resultat->execute([
+            'mail' => $mail,
+        ]);
+        echo 'Ligne modifier';
+    } catch (Exception $ex) {
+        die('Erreur : ' . $ex->getMessage());
+    }
+}
+
+function RemoveConsentement($mail){
+    $req = "DELETE FROM NEWSLETTER"
+           ."where mail = :mail";
+    try{
+        $nolarkBD = new PDO(
+                'mysql:host=localhost;dbname=nolark;charset=utf8',
+                'nolarkadmin',
+                'nolarkadmin'
+        );
+        
+        $resultat = $nolarkBD ->prepare($req);
+        $resultat->execute([
+            'mail' => $mail,
+        ]);
+        echo 'Ligne modifier';
+    } catch (Exception $ex) {
+        die('Erreur : ' . $ex->getMessage());
+    }
+}
